bd6e82bc7868d3ef54c78dba5e734b9915db3238
Mina Coniglio <minaconiglio@Mina-Coniglios-MacBook-Pro.local>
2018-01-06 15:40:35 -0500

    updating changes to program, so print page will show all available QR codes of database clues

--- app.js
+++ app.js
@@ -10,10 +10,8 @@ var cookieParser = require('cookie-parser')
 
 
 app.use(cookieParser())
-
-app.use(cookieSession({
-  secret: '1234567890QWERTY'
-}))
+app.use(cookieSession({secret: '1234567890QWERTY'}))
+app.use('/static', express.static(__dirname + '/static'));//try to figure out what is going on with the static links
 
 // Add headers
 app.use(function (req, res, next) {
@@ -36,69 +34,59 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/static', express.static(__dirname + '/static'));//try to figure out what is going on with the static links
-
 app.get('/read_all', function (req, res) {
-
-   if(req.session.lastPage) {
-      console.log('Last page was: ' + req.session.lastPage + '. ');
-   }
-   req.session.lastPage = '/read_all'
-
-   jsonfile.readFile( "data.json", 'utf8', function (err, data) {
-      res.write( JSON.stringify(data, null, 4) );
-      console.log('read_all sent ...');
-      console.log('Cookies: ', req.cookies)
-    });
+  req.session.lastPage = '/read_all'
+  if(req.session.lastPage) {
+    console.log('Most recent request was: ' + req.session.lastPage + '. ');
+  }
+  jsonfile.readFile( "data.json", 'utf8', function (err, data) {
+    //res.write( JSON.stringify(data, null, 4) );
+    res.json(data);
+    console.log('read_all sent ...');
+    //console.log('Cookies: ', req.cookies)
+  });
 });
 
 app.get('/page', function (req, res) {
-
-  if(req.session.lastPage) {
-      console.log('Last page was: ' + req.session.lastPage + '. ');
-  }
   req.session.lastPage = '/page'
-
+  if(req.session.lastPage) {
+    console.log('Most recent request was: ' + req.session.lastPage + '. ');
+  }
   res.sendFile(path.join(__dirname + '/index.html'));
   console.log('page sent ...');
-  console.log('Cookies: ', req.cookies)
+  //console.log('Cookies: ', req.cookies)
 });
 
 app.get('/print', function (req, res) {
-
-    if(req.session.lastPage) {
-        console.log('Last page was: ' + req.session.lastPage + '. ');
-    }
-    req.session.lastPage = '/print'
-
+  req.session.lastPage = '/print'
+  if(req.session.lastPage) {
+    console.log('Most recent request was: ' + req.session.lastPage + '. ');
+  }
   res.sendFile(path.join(__dirname + '/pages/print.html'));
   console.log('print sent ...');
-  console.log('Cookies: ', req.cookies)
+  //console.log('Cookies: ', req.cookies)
 });
 
 app.get('/:key', function (req, res) {
-
-   if(req.session.lastPage) {
-      console.log('Last page was: ' + req.session.lastPage + '. ');
+  req.session.lastPage = req.params.key
+  if(req.session.lastPage) {
+    console.log('Most recent request was: ' + req.session.lastPage + '. ');
    }
-   req.session.lastPage = '/' + req.params.key
-
   if (req.params.key.slice(0,5) == 'code:'){
     var code_text;
     var key_string = req.params.key.replace(/code:/g,'')
     var data = jsonfile.readFileSync( "data.json", 'utf8')
     for (var i = 0; i < data.length; i++) {
-        if (data[i].key === key_string) {
-          code_text = req.protocol + '://' + req.get('host') + '/' + key_string;
-          break;
+      if (data[i].key === key_string) {
+        code_text = req.protocol + '://' + req.get('host') + '/' + key_string;
+        break;
       } else {
-          code_text = key_string
+        code_text = key_string
       }
     };
-      var code = qr.image(code_text, { type: 'svg' })
-      res.type('svg');
-      code.pipe(res);
-
+    var code = qr.image(code_text, { type: 'svg' })
+    res.type('svg');
+    code.pipe(res);
     } else {
       jsonfile.readFile( "data.json", 'utf8', function (err, data) {
         for (var i = 0; i < data.length; i++) {
@@ -108,10 +96,11 @@ app.get('/:key', function (req, res) {
         };
       });
     }
-  console.log('Cookies: ', req.cookies)
+  //console.log('Cookies: ', req.cookies)
 });
 
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
-    console.log('EDOC_RQ is running on http://localhost:' + port);
+  console.log('EDOC_RQ is running on http://localhost:' + port);
 });
