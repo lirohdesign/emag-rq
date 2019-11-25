acb3ed378b78819885c0f205d27fff483d8052fb
Mina Coniglio <minaconiglio@Mina-Coniglios-MacBook-Pro.local>
2019-11-24 23:31:09 -0500

    trying to fix crypt issues4

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var env = process.env.NODE_ENV || 'production';
 var config = require('./config')[env];
 
 //var urlCrypt = require('url-crypt')(config.crypt_key);
-var simpleCrypto = require('simple-cypto-js')(config.crypt_key);
+import SimpleCrypto from "simple-crypto-js"
+var simpleCrypto = new SimpleCrypto(config.crypt_key);
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
