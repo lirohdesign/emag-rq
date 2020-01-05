e092294ce7ac04db7d138c17dc8758a6dc69be68
Mina Coniglio <minaconiglio@Mina-Coniglios-MacBook-Pro.local>
2020-01-05 00:31:45 -0500

    reading redis

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ if (req.params.key.slice(0,5) == 'code:') {
     }
 
     //this section creates pages from template.pug based on the URL key
-    client.hgetall(req.clientIp, gameID, clueID, function(err, usr_pg_view){
+    client.hgetall(req.clientIp, req.params.key, function(err, usr_pg_view){
         console.dir('redis data log:' + usr_pg_view);
         jsonfile.readFile( "data.json", 'utf8', function (err, data) {
           var pg_json_record = {}
@@ -115,8 +115,8 @@ if (req.params.key.slice(0,5) == 'code:') {
           });
         });
     });
-    console.log(game_call);
-    client.hmset(req.clientIp, gameID, clueID, Date())
+    console.log(req.params.key);
+    client.hmset(req.clientIp, req.params.key, Date())
   }
 
   client.quit()
