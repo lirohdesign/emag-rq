c21952292486ef59e8b43aa67893b96c0897d3e9
Mina Coniglio <minaconiglio@Mina-Coniglios-MacBook-Pro.local>
2020-01-16 09:22:34 -0500

    attempting to post direct from cognito form into mongo

--- app.js
+++ app.js
@@ -119,20 +119,20 @@ app.get('/', function(req, res){
           } else {res.send(JSON.stringify({error : 'Error'}))}
     })
 });
+//
+//const bodyParser = require('body-parser');
+//var assert = require('assert')
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-const bodyParser = require('body-parser');
-var assert = require('assert')
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/post-test', (req, res) => {
-    EmagrqModel.collection.insert(req.body, function(err, r){
-      assert.equal(null, err);
-      assert.equal(r);
-      db.close();
-    })
-    console.log('Got body:', req.body);
-    res.sendStatus(200);
-});
+//app.post('/post-test', (req, res) => {
+//    EmagrqModel.collection.insert(req.body, function(err, r){
+//      assert.equal(null, err);
+//      assert.equal(r);
+//      db.close();
+//    })
+//    console.log('Got body:', req.body);
+//    res.sendStatus(200);
+//});
 
 
 
