8c8576d14114900d319ffc86e6bde6cf744dd113
Benjamin Shelby <benjamin.j.shelby@gmail.com>
2020-03-31 18:14:44 -0400

    adding the builder form and linking it to the database

--- config.js
+++ config.js
@@ -24,62 +24,24 @@ production: {
     crypt_key: "w$^DhPeB$Hu&*t3xT87KdBjVaNzKE%NGYw7sPxRdWFp4"
   },
 cognito_schema: {
-  _id: String,
-  Form: {
-    Id: String,
-    InternalName: String,
-    Name: String
-  },
-//  $version: ,
-//  $etag: ,
-  Gamename: String,
-  Startlocation: String,
-  DescriptionForStart: String,
-  Gamelogic: String,
-  Email: String,
-//  Id: String,
-  Entry: {
-    AdminLink: String,
-    CustomerCard: String,
-    DateCreated: Date,
-    DateSubmitted: Date,
-    DateUpdated: Date,
-    EditLink: String,
-    LastPageViewed: String,
-    Number: Number,
-    Order: String,
-    Origin: {
-      City: String,
-      CountryCode: String,
-      IpAddress: String,
-      IsImported: String,
-      Region: String,
-      Timezone: String,
-      UserAgent: String
-      },
-    PaymentToken: String,
-    Status: String,
-    Timestamp: Date,
-    Version: Number,
-    ViewLink: String
-    },
-  GameData: [
-    {
-      Id: String,
-      CodeLocation: String,
-      CluesToFindThisItem: [
-        {
-          Id: String,
-          Description: String,
-          ItemNumber: Number
-        }
-      ],
-      Timeallocated: Number,
-      Photolink: String,
-      ItemNumber: Number
-    }
-  ]
-}
+    _id: String,
+    game_name: String,
+    user_name: String,
+    description_for_start: String,
+    logic: String,
+    date_created: Date,
+    qr_codes: [{
+      stamp: Date,
+      hint: String,
+      time_allocated: Number,
+      photo_link: String,
+      location: String,
+      details: [{
+          clue: String,
+          stamp: Date
+      }]
+    }]
+  }
 };
 module.exports = config;
 
